eda5cce170b48eb065f92a2ca2fe6520658243d3
kristina kirby <kristinakirby@mac.com>
2018-06-15 10:27:37 -0600

    second commit

--- fridaywk6bt.rb
+++ fridaywk6bt.rb
@@ -1,30 +1,34 @@
-#require gem pry for debugging
 require 'pry'
+require 'colorize'
 
-#create a function named find_repeat
-def find_repeat
-#create instance variable @latters with a value of hash 
-@letters = Hash.new 0
-#print out message to user
-puts "type something!"
-#getting input from user and store it in local variable input
- input = gets.strip
-#.split(“”) splits every latter of a word and returns array of letters , .sort sorts the array elements 
- .split('').sort
-#loops through each character of input string
- input.each do |character| #defining the object
-   # for each character of letters adds 1 
- @letters [character] += 1
-# end of loop
- end
-#prints letters
- puts @letters
-#loops through each element of letters hash and takes its key and value
- @letters.each do |key, value|
-   puts "Nice! The letter #{key} is in your answer #{value} times!"
- end
-end 
+def menu
+  print `clear`
+  puts "---Letters---".yellow
+  @entry = gets.strip
+  letter_check
+end
 
-#calling find_repeat function
-find_repeat
+def letter_check
+  most_repeats = ''
+  most = 0
+  words = @entry.split(/\W/)
+  words.each do |word|
+    letters = word.length
+    letters_uniq = word.split("").uniq.length
+    repeats = letters - letters_uniq
+    if repeats > most
+      most = repeats
+      most_repeats = word
+    elsif
+      repeats == most
+      most_repeats = most_repeats + ", " + word
+    end
+  end
+  if most == 0
+    puts "No words with repeated letters"
+  else
+    puts most_repeats + " has/have the most repeated letters."
+  end
+end
 
+menu
